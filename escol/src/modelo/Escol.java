b8becffe07adf1fa8979ebf8716cee8161ffa928
Administrador <Administrador@10.10.4.16>
2019-09-12 16:51:14 -0300

    Crie um pacote controlador (botão direito em Pacotes de Códigos-fonte → Novo → Pacote Java) Criar a Classe ControladorNomeEntidade no pacote controlador De volta na Manutenção, adicionar evento no botão Adicionar (dois cliques no botão Adicionar → inserir a linha abaixo → Clicar na Lâmpada → Clicar em Criar método inserir) controlador.ControladorNomeEntidade.inserir(this);

---
+++ escol/src/modelo/Escol.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Escol {
+    private int codigo;
+    private String nome;
+    private String endereco;
+    private String sigla;
+    private int nalunos;
+    private Double area;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public int getNalunos() {
+        return nalunos;
+    }
+
+    public void setNalunos(int nalunos) {
+        this.nalunos = nalunos;
+    }
+
+    public Double getArea() {
+        return area;
+    }
+
+    public void setArea(Double area) {
+        this.area = area;
+    }
+
+    @Override
+    public String toString() {
+        return "Escol{" + "nome=" + nome + '}';
+    }
+    
+}
